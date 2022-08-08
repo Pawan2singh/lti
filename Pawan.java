dceefac47fe9e9d440169ddd6fdaea9c685d1408
Pawan2singh <110519407+Pawan2singh@users.noreply.github.com>
2022-08-08 11:56:31 +0530

    Update Pawan.java
    3rd change

--- Pawan.java
+++ Pawan.java
@@ -1,6 +1,6 @@
 public class Pawan{
   public static void main(String args[]){
   System.out.println("Heyyy pawan");
-    System.out.println("Heyyy pawan 2.0");
+    System.out.println("Heyyy pawan 2.0 !! how u doinnnn!!");
   }
 }
